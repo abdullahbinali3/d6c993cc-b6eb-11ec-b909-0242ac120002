f1b8d70fccc0a43d823386a41671058d487cc9a0
Abdullah Ali <abdullah.ali@smsglobal.com>
2022-04-16 01:38:24 +1000

    tests setup

---
+++ tests/Feature/reportTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class reportTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testSuccess()
+    {
+        // tests if command has been successfully executed
+        $this->artisan('inspire')->assertSuccessful();
+    }
+
+    public function testSuccess()
+    {
+        // tests if command has been successfully executed
+        $this->artisan('inspire')->assertSuccessful();
+    }
+
+
+}
